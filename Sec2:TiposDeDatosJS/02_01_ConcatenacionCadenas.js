3171c07666ccdf69f554e2ca93558ee3bb40a4ba
neithaltair <neith.altair.na@gmail.com>
2021-02-09 14:43:44 -0500

    CREATE = Concatenaciones de cadenas y numeros

---
+++ Sec2:TiposDeDatosJS/02_01_ConcatenacionCadenas.js
@@ -0,0 +1,16 @@
+var nombre = 'Juan';
+var apellido = 'Roberto';
+
+var nombreCompleto = nombre+' '+apellido;
+console.log(nombreCompleto);
+
+//Concatenar numero a cadena Toma el numero como cadena
+var x = nombre +  9808;
+console.log(x);
+// Concatena nombre sumando numeros
+x = nombre + (2+4);
+console.log(x);
+
+x = 2+8+' Altair'
+console.log(x);
+
